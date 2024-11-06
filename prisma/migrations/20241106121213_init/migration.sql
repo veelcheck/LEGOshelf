02a6950edfabb392f82b8c1ab86fd1d470e8631b
veelcheck <veelcheck@gmail.com>
2024-11-06 13:17:45 +0100

    first migration

---
+++ prisma/migrations/20241106121213_init/migration.sql
@@ -0,0 +1,44 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "legoShelfId" INTEGER,
+    "wishlistId" INTEGER,
+    CONSTRAINT "User_legoShelfId_fkey" FOREIGN KEY ("legoShelfId") REFERENCES "LegoShelf" ("id") ON DELETE SET NULL ON UPDATE CASCADE,
+    CONSTRAINT "User_wishlistId_fkey" FOREIGN KEY ("wishlistId") REFERENCES "Wishlist" ("id") ON DELETE SET NULL ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "LegoSet" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "imagePath" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "parts" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "LegoShelf" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "totalParts" INTEGER NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Wishlist" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "userId" INTEGER NOT NULL,
+    "legoSetId" INTEGER NOT NULL,
+    CONSTRAINT "Wishlist_legoSetId_fkey" FOREIGN KEY ("legoSetId") REFERENCES "LegoSet" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_legoShelfId_key" ON "User"("legoShelfId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_wishlistId_key" ON "User"("wishlistId");
