4800d68c0ea52c1b66356a0d5ec3d9ebf56aa06d
veelcheck <veelcheck@gmail.com>
2024-11-27 11:32:20 +0100

    changing logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 import Button from "./Button";
 import logo from "../public/images/logo.png";
 import { useAuth } from "../context/authContext";
-import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 
 export default function Header() {
@@ -16,9 +15,15 @@ export default function Header() {
     <header className="flex flex-col items-center justify-between gap-4 bg-lego-yellow px-10 py-6 sm:flex-row sm:gap-0">
       <Link
         href="/"
-        className="w-fit cursor-pointer rounded border-4 border-black px-4 py-2"
+        className="h-fit w-fit cursor-pointer rounded-md bg-lego-red"
       >
-        <Image src={logo} alt="lego-shelf logo" priority={true}></Image>
+        <Image
+          src={logo}
+          alt="legoshelf logo"
+          priority={true}
+          height={93}
+          width={200}
+        ></Image>
       </Link>
       {isDashboard ? (
         <Link href="/">
