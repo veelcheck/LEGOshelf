3e3e60587610081facabf495d16f9f3651ba0fb8
veelcheck <veelcheck@gmail.com>
2025-03-18 09:39:31 +0100

    logo changed | next upgrade to v15

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,15 +5,17 @@ import logo from "../public/images/logo.png";
 
 export default function Header() {
   return (
-    <header className="flex flex-col items-center justify-between gap-4 bg-lego-yellow px-10 py-6 sm:flex-row sm:gap-0">
-      <Link
-        href="/"
-        className="w-fit cursor-pointer rounded border-4 border-black px-4 py-2"
-      >
-        <Image src={logo} alt="lego-shelf logo" priority={true}></Image>
+    <header className="flex flex-col items-center justify-between bg-lego-yellow px-10 sm:flex-row sm:gap-0">
+      <Link href="/" className="cursor-pointer">
+        <Image
+          src={logo}
+          alt="lego-shelf logo"
+          priority={true}
+          className="max-h-[160px] max-w-[200px]"
+        ></Image>
       </Link>
       <Link href="/your-shelf">
-        <Button>Log shelf</Button>
+        <Button className="mb-4 sm:mb-0">Log shelf</Button>
       </Link>
     </header>
   );
