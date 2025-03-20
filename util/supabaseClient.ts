323fa5151e5740ec6f0b9ff914911ba3c97daa6c
veelcheck <veelcheck@gmail.com>
2025-03-20 19:45:05 +0100

    import "server-only" for supabase client - for vercel

--- util/supabaseClient.ts
+++ util/supabaseClient.ts
@@ -1,4 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
+import "server-only"; // Ensures it never runs on the client
+
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL! as string;
 const supabaseKey = process.env.NEXT_SUPABASE_ANON_KEY! as string;
 export const supabase = createClient(supabaseUrl, supabaseKey);
